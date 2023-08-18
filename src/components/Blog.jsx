36bac1e31dcceaa3482360e75f1307e63adacb1f
sumit kumar singh <sumitksingh1166@gmail.com>
2023-08-18 09:22:31 +0530

    blog

---
+++ src/components/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div>
+            <h3 className='text-center'>Blog</h3>
+            <hr />
+        </div>
+    )
+}
+
+export default Blog;
